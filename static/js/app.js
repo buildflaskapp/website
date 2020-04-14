f638f525b3a583ae0b9423b171bf25c10c7d234c
kouul <maulloohans@gmail.com>
2020-04-14 17:16:00 +0400

    init

---
+++ static/js/app.js
@@ -0,0 +1,40 @@
+var termxBuildFlaskApp = document.querySelector('.termX-build-flask-app');
+
+var typewriter = new Typewriter(termxBuildFlaskApp, {
+    // cursor: " "
+});
+
+typewriter.typeString('$ build-flask-app hello-world' + '<br/>$ ')
+    .pauseFor(1000)
+    .typeString('cd hello-world' + '<br/>$ ')
+    .pauseFor(1500)
+    .typeString('python3 app.py ')
+    .start();
+
+function checkCDN(){
+    var theString = "build-flask-app hello-world";
+
+    if (document.querySelector('.bootstrap-check').checked === true){
+        theString = theString + ' -bs';
+    }
+    
+    if (document.querySelector('.gsap-check').checked === true){
+        theString = theString + ' -gsap';
+    }
+
+    if (document.querySelector('.jquery-check').checked === true){
+        theString = theString + ' -jq';
+    }
+
+    if (document.querySelector('.fontawesome-check').checked === true){
+        theString = theString + ' -fa';
+    }
+
+    if (document.querySelector('.docker-check').checked === true){
+        theString = theString + ' -dc';
+        document.querySelector('.usage-runApp').innerHTML = 'docker-compose up -d';
+    } else{
+        document.querySelector('.usage-runApp').innerHTML = 'python3 app.py';
+    }
+    document.querySelector('.usage-build-flask-app').innerHTML = theString;
+}
