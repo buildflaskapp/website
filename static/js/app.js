d60ad86b40d42d8cadd6443b6040c4651ac2033c
kouul <maulloohans@gmail.com>
2020-04-15 20:33:21 +0400

    added animation to navbar

--- static/js/app.js
+++ static/js/app.js
@@ -11,34 +11,45 @@ typewriter.typeString('$ build-flask-app hello-world' + '<br/>$ ')
     .typeString('python3 app.py ')
     .start();
 
-function checkCDN(){
+function checkCDN() {
     var theString = "build-flask-app hello-world";
 
-    if (document.querySelector('.bootstrap-check').checked === true){
+    if (document.querySelector('.bootstrap-check').checked === true) {
         theString = theString + ' -bs';
     }
-    
-    if (document.querySelector('.gsap-check').checked === true){
+
+    if (document.querySelector('.gsap-check').checked === true) {
         theString = theString + ' -gsap';
     }
 
-    if (document.querySelector('.jquery-check').checked === true){
+    if (document.querySelector('.jquery-check').checked === true) {
         theString = theString + ' -jq';
     }
 
-    if (document.querySelector('.fontawesome-check').checked === true){
+    if (document.querySelector('.fontawesome-check').checked === true) {
         theString = theString + ' -fa';
     }
 
-    if (document.querySelector('.debugger-check').checked === true){
+    if (document.querySelector('.debugger-check').checked === true) {
         theString = theString + ' -d';
     }
 
-    if (document.querySelector('.docker-check').checked === true){
+    if (document.querySelector('.docker-check').checked === true) {
         theString = theString + ' -dc';
         document.querySelector('.usage-runApp').innerHTML = 'docker-compose up -d';
-    } else{
+    } else {
         document.querySelector('.usage-runApp').innerHTML = 'python3 app.py';
     }
     document.querySelector('.usage-build-flask-app').innerHTML = theString;
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar").style.top = "0";
+    } else {
+        document.querySelector(".navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
 }
