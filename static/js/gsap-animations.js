6c693dede752f85de7cbe5ccb6a84b367b1fb39c
kouul <maulloohans@gmail.com>
2020-04-19 14:17:53 +0400

    added gsap animation to home page

---
+++ static/js/gsap-animations.js
@@ -0,0 +1,13 @@
+gsap.from(".navbar", {duration: 0.5, y: -100, opacity: 0, scale: 1});
+
+gsap.from(".ourBrand", {duration: 0.5, delay:0.5, y: -20, opacity: 0, scale: 1});
+
+gsap.from(".brand-logo", {duration: 0.5, delay: 0.7, opacity: 0, scale: 1});
+
+gsap.from(".nav-link", {duration: 0.5, delay:0.8, y: -10, stagger: 0.2, opacity: 0, scale: 1});
+
+gsap.from(".main-title", {duration: 0.5, delay: 2.0, y: 10, opacity: 0, scale: 1});
+
+gsap.from(".title-info", {duration: 0.5, delay: 2.4, y: -10, opacity: 0, scale: 1});
+
+gsap.from(".code-to-run", {duration: 0.5, delay: 2.8, opacity: 0, scale: 1});
